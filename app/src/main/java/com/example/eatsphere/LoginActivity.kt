5d8c73b5b26f09bb7ea3e6b44eb0c8d42153b6df
VAIBHAV KUMAWAT <97437528+vaibhavk10@users.noreply.github.com>
2024-08-18 20:30:44 +0530

    updated

--- app/src/main/java/com/example/eatsphere/LoginActivity.kt
+++ app/src/main/java/com/example/eatsphere/LoginActivity.kt
@@ -19,7 +19,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.loginbutton.setOnClickListener {
-            val intent = Intent(this, SignActivity::class.java)
+            val intent = Intent(this, ChooseLocationActivity::class.java)
             startActivity(intent)
         }
 
